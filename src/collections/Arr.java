9f54060ccaef32c2d839561ca238bdd39e8ffe03
VENKAT_CHOWDHARPALLY <VENKAT_CHOWDHARPALLY@DESKTOP-FPI0PK7>
2019-07-13 11:22:59 +0530

    version 2

---
+++ src/collections/Arr.java
@@ -0,0 +1,18 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Arr {
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		a.add(1);
+		a.add("fgsjhj");
+		a.add("e");
+		a.add(1);
+		a.add("fgsjhj");
+		a.add("e");
+		
+		System.out.println(a);
+	}
+
+}
